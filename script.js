3a4651dc8ba8d2f47483bfb9f58b8c77191176f3
Benjamin Rossario A <benrobotics74@gmail.com>
2024-06-19 14:45:14 +0530

    updated JavaScript

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 const inputUser = document.getElementById("text-input");
-const checkbuttonUser = document.getElementById("check-btn");
+const checkButtonUser = document.getElementById("check-btn");
 const resultDiv = document.getElementById("result");
 let inputUserValue = inputUser.value;
 
 const checkPalindrome = (input) => {
   const givenInput = input;
   if (input == "") {
-    document.alert("Please input a value");
+    alert("Please input a value"); // done to ensure that the input is not empty.
     return;
   }
 
-  const normalInput = input.replace(/[^a-zA-Z0-9]/gi, "").toLowerCase();
-  const reversedInput = input.split("").reverse().join("");
-  if (normalInput === reversedInput) {
+  resultDiv.replaceChildren(); // done to remove the previous result of palindrome.
+
+  const normalInput = input.replace(/[^A-Za-z0-9]/gi, "").toLowerCase(); // done to remove the punctuations
+  const reversedInput = normalInput.split("").reverse().join(""); // done to reverse the userinput value
+  const pValue = document.createElement("p");
+  pValue.style.fontSize = "18px";
+
+  pValue.className = "output";
+  if (normalInput == reversedInput) {
+    pValue.innerHTML = `${givenInput} is a palindrome`;
+  } else {
+    pValue.innerHTML = `${givenInput} is not a palindrome`;
   }
+  resultDiv.appendChild(pValue);
+  resultDiv.classList.remove("hidden");
 };
+
+checkButtonUser.addEventListener("click", () => {
+  checkPalindrome(inputUser.value);
+  inputUser.value = "";
+});
+
+inputUser.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    checkPalindrome(inputUser.value);
+    inputUser.value = "";
+  }
+});
